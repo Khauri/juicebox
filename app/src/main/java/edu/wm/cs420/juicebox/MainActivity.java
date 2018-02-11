e125b9c51005c1deed019f40ff244dcc8f271030
Khauri <kmcclain@email.wm.edu>
2018-02-11 10:28:20 -0500

    Basic interface. Created basic navigation workflow utilizing viewpager, fragments, and bottom navigation view.

--- app/src/main/java/edu/wm/cs420/juicebox/MainActivity.java
+++ app/src/main/java/edu/wm/cs420/juicebox/MainActivity.java
@@ -1,13 +1,114 @@
 package edu.wm.cs420.juicebox;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity
+        extends AppCompatActivity
+        implements QueueFragment.OnFragmentInteractionListener,
+        SearchFragment.OnFragmentInteractionListener,
+        SocialFragment.OnFragmentInteractionListener {
+
+    private static final int NUM_ITEMS = 3;
+
+    private ViewPager mPager;
+    private MyAdapter mAdapter;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // set up adapter and view pager
+        mAdapter = new MyAdapter(getSupportFragmentManager());
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav);
+        // Change fragment on click
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        return loadFragment(item.getItemId());
+                    }
+                }
+        );
+        // change menu item on scroll
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                // change selected menu item
+                // be careful of recursive accidents
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        mPager.setCurrentItem(1);
+    }
+
+    private boolean loadFragment(int id){
+        //Fragment fragment = null;
+        switch(id){
+            case R.id.bottombar_search:
+                mPager.setCurrentItem(0, true);
+                break;
+            case R.id.bottombar_queue:
+                mPager.setCurrentItem(1, true);
+                break;
+            case R.id.bottombar_social:
+                mPager.setCurrentItem(2, true);
+                break;
+            default:
+                return false;
+
+        }
+        return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        // Does nothing but is required
+    }
+
+    public static class MyAdapter extends FragmentPagerAdapter {
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position){
+                case 0:
+                    return SearchFragment.newInstance("Hello", "World");
+                case 1:
+                    return QueueFragment.newInstance("Hello", "World");
+                case 2:
+                    return SocialFragment.newInstance("Hello", "World");
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
     }
 }
