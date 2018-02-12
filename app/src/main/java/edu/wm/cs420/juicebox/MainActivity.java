4478cd93af1252c5fd1c66c39e30b6ec837074af
Jill <jjmao@email.wm.edu>
2018-02-11 21:01:36 -0500

    Added spotify authentication library

--- app/src/main/java/edu/wm/cs420/juicebox/MainActivity.java
+++ app/src/main/java/edu/wm/cs420/juicebox/MainActivity.java
@@ -1,5 +1,10 @@
 package edu.wm.cs420.juicebox;
 
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -11,24 +16,40 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 public class MainActivity
         extends AppCompatActivity
         implements QueueFragment.OnFragmentInteractionListener,
         SearchFragment.OnFragmentInteractionListener,
-        SocialFragment.OnFragmentInteractionListener {
+        SocialFragment.OnFragmentInteractionListener{
+        //SpotifyPlayer.NotificationCallback, ConnectionStateCallback{
 
     private static final int NUM_ITEMS = 3;
+
+    private static final String CLIENT_ID = "6098304025324b66af007d562d58830e";
+    // TODO: Replace with your redirect URI
+    private static final String REDIRECT_URI = "yourcustomprotocol://callback";
 
     private ViewPager mPager;
     private MyAdapter mAdapter;
     private BottomNavigationView bottomNavigationView;
+    private static final int REQUEST_CODE = 1337;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Authentifications
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+
+
+
         // set up adapter and view pager
         mAdapter = new MyAdapter(getSupportFragmentManager());
         mPager = (ViewPager) findViewById(R.id.pager);
@@ -47,7 +68,7 @@ public class MainActivity
         mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+                // Do nothing
             }
 
             @Override
@@ -58,10 +79,38 @@ public class MainActivity
 
             @Override
             public void onPageScrollStateChanged(int state) {
-
+                // Do nothing
             }
         });
         mPager.setCurrentItem(1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        Log.d("Juicebox", "Recieved activity result");
+//
+//        // Check if result comes from the correct activity
+//        // The next 19 lines of the code are what you need to copy & paste! :)
+//        if (requestCode == REQUEST_CODE) {
+//            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+//            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+//                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+//                Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+//                    @Override
+//                    public void onInitialized(SpotifyPlayer spotifyPlayer) {
+//                        mPlayer = spotifyPlayer;
+//                        mPlayer.addConnectionStateCallback(MainActivity.this);
+//                        mPlayer.addNotificationCallback(MainActivity.this);
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable throwable) {
+//                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+//                    }
+//                });
+//            }
+//        }
     }
 
     private boolean loadFragment(int id){
@@ -111,4 +160,31 @@ public class MainActivity
             return NUM_ITEMS;
         }
     }
+
+//    // Authentification callbacks
+//    @Override
+//    public void onLoggedIn() {
+//        Log.d("MainActivity", "User logged in");
+//    }
+//
+//    @Override
+//    public void onLoggedOut() {
+//        Log.d("MainActivity", "User logged out");
+//    }
+//
+//    @Override
+//    public void onLoginFailed(Error var1) {
+//        Log.d("MainActivity", "Login failed");
+//    }
+//
+//    @Override
+//    public void onTemporaryError() {
+//        Log.d("MainActivity", "Temporary error occurred");
+//    }
+//
+//    @Override
+//    public void onConnectionMessage(String message) {
+//        Log.d("MainActivity", "Received connection message: " + message);
+//    }
+//}
 }
