d5b5f5b87195dba5ef4874e53a76c1d47d9f83d7
Jill <jjmao@email.wm.edu>
2018-02-11 21:52:22 -0500

    Added spotify player library

--- app/src/main/java/edu/wm/cs420/juicebox/MainActivity.java
+++ app/src/main/java/edu/wm/cs420/juicebox/MainActivity.java
@@ -3,6 +3,13 @@ package edu.wm.cs420.juicebox;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.SpotifyPlayer;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -23,8 +30,8 @@ public class MainActivity
         extends AppCompatActivity
         implements QueueFragment.OnFragmentInteractionListener,
         SearchFragment.OnFragmentInteractionListener,
-        SocialFragment.OnFragmentInteractionListener{
-        //SpotifyPlayer.NotificationCallback, ConnectionStateCallback{
+        SocialFragment.OnFragmentInteractionListener,
+        SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
 
     private static final int NUM_ITEMS = 3;
 
@@ -35,7 +42,9 @@ public class MainActivity
     private ViewPager mPager;
     private MyAdapter mAdapter;
     private BottomNavigationView bottomNavigationView;
+    // Spotify stuff
     private static final int REQUEST_CODE = 1337;
+    private Player mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,28 +98,28 @@ public class MainActivity
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
         Log.d("Juicebox", "Recieved activity result");
-//
-//        // Check if result comes from the correct activity
-//        // The next 19 lines of the code are what you need to copy & paste! :)
-//        if (requestCode == REQUEST_CODE) {
-//            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
-//            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
-//                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
-//                Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
-//                    @Override
-//                    public void onInitialized(SpotifyPlayer spotifyPlayer) {
-//                        mPlayer = spotifyPlayer;
-//                        mPlayer.addConnectionStateCallback(MainActivity.this);
-//                        mPlayer.addNotificationCallback(MainActivity.this);
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable throwable) {
-//                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
-//                    }
-//                });
-//            }
-//        }
+
+        // Check if result comes from the correct activity
+        // The next 19 lines of the code are what you need to copy & paste! :)
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+                    @Override
+                    public void onInitialized(SpotifyPlayer spotifyPlayer) {
+                        mPlayer = spotifyPlayer;
+                        mPlayer.addConnectionStateCallback(MainActivity.this);
+                        mPlayer.addNotificationCallback(MainActivity.this);
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                    }
+                });
+            }
+        }
     }
 
     private boolean loadFragment(int id){
@@ -137,6 +146,46 @@ public class MainActivity
         // Does nothing but is required
     }
 
+    @Override
+    public void onLoggedIn() {
+        mPlayer.playUri(null, "spotify:track:2TpxZ7JUBn3uw46aR7qd6V", 0, 0);
+    }
+
+    @Override
+    public void onLoggedOut() {
+
+    }
+
+    @Override
+    public void onLoginFailed(Error error) {
+
+    }
+
+    @Override
+    public void onTemporaryError() {
+
+    }
+
+    @Override
+    public void onConnectionMessage(String s) {
+
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+        Log.d("MainActivity", "Playback error received: " + error.name());
+        switch (error) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
     public static class MyAdapter extends FragmentPagerAdapter {
         public MyAdapter(FragmentManager fm) {
             super(fm);
@@ -160,31 +209,4 @@ public class MainActivity
             return NUM_ITEMS;
         }
     }
-
-//    // Authentification callbacks
-//    @Override
-//    public void onLoggedIn() {
-//        Log.d("MainActivity", "User logged in");
-//    }
-//
-//    @Override
-//    public void onLoggedOut() {
-//        Log.d("MainActivity", "User logged out");
-//    }
-//
-//    @Override
-//    public void onLoginFailed(Error var1) {
-//        Log.d("MainActivity", "Login failed");
-//    }
-//
-//    @Override
-//    public void onTemporaryError() {
-//        Log.d("MainActivity", "Temporary error occurred");
-//    }
-//
-//    @Override
-//    public void onConnectionMessage(String message) {
-//        Log.d("MainActivity", "Received connection message: " + message);
-//    }
-//}
 }
